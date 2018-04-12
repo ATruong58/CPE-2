75ae8632d2b433fb2f1a8855c6d1689dc242a475
Alan <atwpb@mst.edu>
2018-04-11 21:15:16 -0500

    CHANGES THAT KINDA work cool story bro

--- p2.asm
+++ p2.asm
@@ -2,55 +2,60 @@
 
 cseg at 0
 
-	mov 0xA4, #0 ;set Port 2 - bidirectional
-	mov 0x91, #0 ;set Port 1 - bidirectional
-	mov 0x84, #0 ;set Port 0 - bidirectoinal
+	mov 0xA4, #0
+	mov 0x91, #0
+	mov 0x84, #0
 
-	mov r0,#0
+	mov r4, #0
 
 loop:
-	mov c, p0.3 ;sw8
-	jnc up; jump if press to inc
-	cjne r0, #10h, noover; if it doesn't equal 15+1 jump over 15+1 convert
-	sjmp over; is over must wrap
-noover:
-	mov c, p2.2 ;sw9
-	jnc down; jump if press to dec
-	cjne r0, #0FFh, nounder; if it doesn't equal 0-1 jump over 0-1 convert
-	sjmp under; is under must wrap
-nounder:	 
-    sjmp lights; update lights
+	mov c, p0.3
+	jnc up
+	mov c, p2.2
+	jnc down
+	sjmp loop
+
 up:
-	setb p0.3; reset switch ? may work idk
-	inc r0; increment counter
-	sjmp lights; update lights						 
+	setb p0.3
+	inc r4
+	acall delay
+	cjne r4, #10h, lights
+	sjmp over
 down:
-	setb p2.2; rest switch ? may work idk
-	dec r0; decrement counter
-	sjmp lights; update lights
+	setb p2.2
+	dec r4
+	acall delay
+	cjne r4, #0ffh, lights
+	sjmp under
+over:
+	mov r4, #00h
+	acall delay_s
+	sjmp lights
+under:
+	mov r4, #0fh
+	acall delay_s
+	sjmp lights
+
+
 lights:
-	mov a, r0; move counter val to acc
+	mov a, r4; move counter val to acc
 	cpl a; invert for lights
-	rrc a; send last bit to carry for use
+	mov c, 0e0h
+	;rrc a; send last bit to carry for use
 	mov p1.6, c; set light 0
-	rrc a; send last bit to carry for use
+	mov c, 0e1h
 	mov p0.6, c; set light 1
-	rrc a; send last bit to carry for use
+	mov c, 0e2h
 	mov p0.5, c; set light 2
-	rrc a; send last bit to carry for use
+	mov c, 0e3h
 	mov p2.4, c; set light 3
 	sjmp loop; return to loop
-over:
-	mov r0, #00h; reset counter to 0
-	sjmp sound; play sound
-under:
-	mov r0, #0Fh; reset counter to 15
-	sjmp sound; play sound
+	
 sound:
 	; Do sound here
 	mov TMOD, #02h
-	mov TH0, #205
-	mov TL0, #205
+	mov TH0, #255
+	mov TL0, #255
 	setb p1.7; I think this is the speaker
 	clr p2.5;
 	setb TR0
@@ -60,5 +65,44 @@ back:
 	setb p2.5
 	clr TR0
 	clr TF0
-	sjmp lights; update lights
+	sjmp lights
+
+delay_s:
+	mov r0, 255
+	setb p1.7
+d_loop_s:
+	mov r1, 255
+d_loop2_s:
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	djnz r1, d_loop2_s
+	djnz r0, d_loop_s
+	clr p1.7
+	ret
+		
+delay:
+	mov r0, 255
+d_loop:
+	mov r1, 255
+d_loop2:
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	djnz r1, d_loop2
+	djnz r0, d_loop
+	ret
+
 end
