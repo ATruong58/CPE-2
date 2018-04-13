a02d4890d0bee90595629a8d65b402950a63dee8
Alan <atwpb@mst.edu>
2018-04-13 00:38:12 -0500

    added alan comment to codeX

--- p2.asm
+++ p2.asm
@@ -7,18 +7,21 @@ cseg at 0
 	mov 0x84, #0
 
 	mov r4, #0	
-
+	mov r5, #0
+	mov r6, #0
 loop:
 	mov c, p0.3
 	jnc up
 	mov c, p2.2
 	jnc down
-;	mov c, p2.0
-;	jnc ladd
-;	mov c, p0.1
-;	jnc ladd2
-;	mov c, p2.3
-;	jnc countdown
+	mov c, p0.0
+	jnc display_parity
+	mov c, p2.0
+	jnc ladd_jmp
+	mov c, p0.1
+	jnc ladd2_jmp
+	mov c, p2.3
+	jnc rand_jmp
 	sjmp loop
 up:
 	setb p0.3
@@ -39,25 +42,32 @@ under:
 	mov r4, #0fh
 	sjmp delay_s
 
+rand_jmp:
+	ljmp rand
+
+
+ladd_jmp:
+	ljmp ladd
+ladd2_jmp:
+	ljmp ladd2
 lights:
 	mov a, r4; move counter val to acc
-	cpl a; invert for lights
+	cpl a; invert for lights		       
 	mov c, 0e0h
-	;rrc a; send last bit to carry for use
 	mov p1.6, c; set light 0
-	mov c, 0e1h
+	mov c, 0e1h		
 	mov p0.6, c; set light 1
-	mov c, 0e2h
+	mov c, 0e2h		
 	mov p0.5, c; set light 2
-	mov c, 0e3h
+	mov c, 0e3h	
 	mov p2.4, c; set light 3
 	sjmp loop; return to loop
-/*
+;-------ALAN----------------------------------------------------
+;Display register 5 on the binary subject which contain teh added value
 lightsa:
-	mov a, r5; move counter val to acc
+	mov a, r5; move r5 val to acc
 	cpl a; invert for lights
 	mov c, 0e0h
-	;rrc a; send last bit to carry for use
 	mov p1.6, c; set light 0
 	mov c, 0e1h
 	mov p0.6, c; set light 1
@@ -66,7 +76,40 @@ lightsa:
 	mov c, 0e3h
 	mov p2.4, c; set light 3
 	sjmp loop; return to loop
-*/
+;-------------------------------------------------------
+
+;-----CHAU--------------------------------------------------
+display_parity:
+	mov a, r4
+	mov r0, #0 ;increment per 1 bit
+	mov r7, #4 ;decrement counter
+loop_parity:
+	rrc a
+	jc up_parity
+	djnz r7, loop_parity
+parity_zero:
+	mov a, r0
+	mov c, 0e0h
+	jc odd_parity
+	ljmp even_parity
+	
+up_parity:
+	inc r0
+	djnz r7, loop_parity
+	ljmp parity_zero
+
+odd_parity:
+	clr p2.7
+	acall delay
+	setb p2.7
+	ljmp loop
+
+even_parity:
+	clr p0.4
+	acall delay
+	setb p0.4
+	ljmp loop
+;-----------------------------------------------------------
 
 delay_s:
 	mov r0, #255
@@ -82,37 +125,39 @@ here:
 	clr TR1
 	clr TF1
 	djnz r0, sound
-	sjmp lights
-
-/*
-countdown:
-	mov TMOD, #00010000b
-	mov TL1, #-614
-	mov TH1, #-614 shr 8
-	setb TR1
-count_here:
-	jnb TF1, here
-	clr TR1
-	clr TF1
-	acall light
-	djnz r4, countdown
-	sjmp loop
-*/
-
-
-/*
+	ljmp lights
+;------ALAN--------------------------------------------------
 ladd:
-	mov a, r4
+	mov a, r4; move counter value to a 
+	acall delay
 	mov r5, a
-	sjmp loop
+	ljmp loop;jump back to loop for user to choose second number to add
 ladd2:
-	mov a,r4
-	mov r6, a
-	mov a,r5
-	add a, r6
-	sjmp lights
-*/
+	mov a,r4; move coutner value to a
+	acall delay
+	mov r6, a; store the 2nd number in register 6
+	mov a,r5;move r5 to a
+	acall delay; delay to correctly display and store number
+	add a, r6;add the two numver 
+	mov r5, a;move the result in r5
+	ljmp lightsa;move to this function to display the register on the
+		    ;device
+;--------------------------------------------------------------
+;-----Cash--------------------------------------------------
+rand:
+	clr p2.7; turn on indicator light
+	inc r4; increment count
+	cjne r4, #10h, next; reset if we make it to 15
+	mov r4, #00h; 
+next:
+	mov c, p1.4; check for stop
+	setb p2.7; turn off indicator light
+	jnc light_jmp; jump if we stopped
+	sjmp rand; otherwise keep incrementing 
+;-----------------------------------------------------------
 
+light_jmp:
+	ljmp lights
 delay:
 	mov r0, 255
 d_loop:
@@ -130,19 +175,4 @@ d_loop2:
 	djnz r1, d_loop2
 	djnz r0, d_loop
 	ret
-/*
-light:
-	mov a, r4; move counter val to acc
-	cpl a; invert for lights
-	mov c, 0e0h
-	;rrc a; send last bit to carry for use
-	mov p1.6, c; set light 0
-	mov c, 0e1h
-	mov p0.6, c; set light 1
-	mov c, 0e2h
-	mov p0.5, c; set light 2
-	mov c, 0e3h
-	mov p2.4, c; set light 3
-	ret return 
-*/
 end
